3067da8fadcc77f0b0c3515c582db6a68457248b
Davi Duarte <rio.daviduarte@gmail.com>
2025-02-10 11:18:06 -0300

    show media in console

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ let nota1 = 7;
 let nota2 = 8;
 let nota3 = 9;
 
-let media = (nota1 + nota2 + nota3) / 3;
+let media = ((nota1 + nota2 + nota3) / 3).toFixed(2);
 
+let resultado;
 if (media >= 7) {
-    console.log("APROVADO");
+    resultado = "Aprovado com média " + media;
 } else {
-    console.log("REPROVADO");
+    resultado = "Reprovado com média " + media;
 }
+
+console.log(resultado);
