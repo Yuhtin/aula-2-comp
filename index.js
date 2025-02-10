71bf3dcd0a09f8ba2cb2fb96ebd3284b8ea2cd23
Davi Duarte <rio.daviduarte@gmail.com>
2025-02-10 12:51:24 -0300

    add console#log in each note

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-let nota1 = 7;
-let nota2 = 8;
-let nota3 = 9;
-
-let notas = []
-notas.push(nota1, nota2, nota3);
-
+let notas = [7, 8, 9]
 let media = 0;
 
 for (let i = 0; i < notas.length; i++) {
     media += notas[i];
+    console.log('Nota ' + (i + 1) + ': ' + notas[i]);
 }
 
 media = (media / notas.length).toFixed(2);
